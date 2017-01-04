fd849e21f11b019cdffe89c67c2f21dec8b584a2
Leon <Leon@LEONVO>
2017-01-04 17:13:51 +0800

    第一次提交

---
+++ 二叉树/MainForm.vb
@@ -0,0 +1,29 @@
+﻿Public Class MainForm
+    Dim RootNode As TreeNode
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Debug.Print("创建二叉树根")
+        RootNode = New TreeNode(100)
+        Debug.Print("为二叉树添加节点")
+        RootNode.Add(50)
+        RootNode.Add(25)
+        RootNode.Add(30)
+        RootNode.Add(15)
+        RootNode.Add(150)
+        RootNode.Add(125)
+        RootNode.Add(175)
+        'For Index As Integer = 0 To 200
+        '    RootNode.Add(Index)
+        'Next
+
+        Debug.Print("前序遍历")
+        Debug.Print(TreeNode.BeforeOrder(RootNode))
+
+        Debug.Print("中序遍历")
+        Debug.Print(TreeNode.MidOrder(RootNode))
+
+        Debug.Print("后序遍历")
+        Debug.Print(TreeNode.AfterOrder(RootNode))
+    End Sub
+
+End Class
---
+++ 二叉树/TreeNode.vb
@@ -0,0 +1,64 @@
+﻿Public Class TreeNode
+    Public Value As Integer
+    Public Parent As TreeNode
+    Public LeftChild As TreeNode
+    Public RightChild As TreeNode
+
+    Public Sub New(NodeValue As Integer)
+        Value = NodeValue
+    End Sub
+
+    Public Function Add(ChildNode As TreeNode) As Boolean
+        Dim InsTreeNode As TreeNode = Me
+        Do While True
+            If ChildNode.Value < InsTreeNode.Value Then
+                If IsNothing(InsTreeNode.LeftChild) Then
+                    Debug.Print("{0} 添加在了 {1} 的左叶子", ChildNode.Value, InsTreeNode.Value)
+                    InsTreeNode.LeftChild = ChildNode : ChildNode.Parent = InsTreeNode : Return True
+                Else
+                    InsTreeNode = InsTreeNode.LeftChild
+                End If
+            ElseIf ChildNode.Value > InsTreeNode.Value Then
+                If IsNothing(InsTreeNode.RightChild) Then
+                    Debug.Print("{0} 添加在了 {1} 的右叶子", ChildNode.Value, InsTreeNode.Value)
+                    InsTreeNode.RightChild = ChildNode : ChildNode.Parent = InsTreeNode : Return True
+                Else
+                    InsTreeNode = InsTreeNode.RightChild
+                End If
+            Else
+                Return False
+            End If
+        Loop
+        Return False
+    End Function
+
+    Public Function Add(ChildValue As Integer) As Boolean
+        Dim ChildNode As TreeNode = New TreeNode(ChildValue)
+        Return Me.Add(ChildNode)
+    End Function
+
+    Public Shared Function BeforeOrder(InsTreeNode As TreeNode) As String
+        Dim TreePath As String = vbNullString
+        TreePath &= InsTreeNode.Value & " "
+        If Not IsNothing(InsTreeNode.LeftChild) Then TreePath &= BeforeOrder(InsTreeNode.LeftChild) & " "
+        If Not IsNothing(InsTreeNode.RightChild) Then TreePath &= BeforeOrder(InsTreeNode.RightChild) & " "
+        Return Strings.Left(TreePath, TreePath.Length - 1)
+    End Function
+
+    Public Shared Function MidOrder(InsTreeNode As TreeNode) As String
+        Dim TreePath As String = vbNullString
+        If Not IsNothing(InsTreeNode.LeftChild) Then TreePath &= MidOrder(InsTreeNode.LeftChild) & " "
+        TreePath &= InsTreeNode.Value & " "
+        If Not IsNothing(InsTreeNode.RightChild) Then TreePath &= MidOrder(InsTreeNode.RightChild) & " "
+        Return Strings.Left(TreePath, TreePath.Length - 1)
+    End Function
+
+    Public Shared Function AfterOrder(InsTreeNode As TreeNode) As String
+        Dim TreePath As String = vbNullString
+        If Not IsNothing(InsTreeNode.LeftChild) Then TreePath &= AfterOrder(InsTreeNode.LeftChild) & " "
+        If Not IsNothing(InsTreeNode.RightChild) Then TreePath &= AfterOrder(InsTreeNode.RightChild) & " "
+        TreePath &= InsTreeNode.Value & " "
+        Return Strings.Left(TreePath, TreePath.Length - 1)
+    End Function
+
+End Class
